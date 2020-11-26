c987493cfc34e4bad573e5facbb650be7872fffb
Ahmad-script <75048922+Ahmad-script@users.noreply.github.com>
2020-11-26 19:28:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const info = require("./lib/info.js");
 /////////////////
 const BotName = 'Bot FranChou'; 
 const instagram = 'https://www.instagram.com/thunder_firee/'; 
-const telegram = '-'; 
+const telegram = 'belum ada'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
 const youtube = 'https://www.youtube.com/channel/UCa1-IREtjmrI5puID5TFhvg';
 //const grupch1 = 'https://chat.whatsapp.com/FJzerGK13m98gflQKqbW52'; 
@@ -64,7 +64,7 @@ fs.existsSync('./session.json') && conn.loadAuthInfo('./session.json')
 //conn.connectOptions.agent = ProxyAgent ('http://1.0.180.120:8080')
 conn.connect();
 
-conn.on('user-presence-update', json => console.log(json.id + ' presence is => ' + json.type)) || console.log('Bot by ig:@reuz.id')
+conn.on('user-presence-update', json => console.log(json.id + ' presence is => ' + json.type)) || console.log('Bot by @Thunder')
 conn.on('message-status-update', json =>
 {
    const participant = json.participant ? ' (' + json.participant + ')' : '' // participant exists when the message is from a group
